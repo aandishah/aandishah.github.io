7e1aa8c5bd6b00743a59bf7c737e4e5b652d2633
aandishah <aandishah@gmail.com>
2021-09-26 21:22:29 -0400

    Completed Task 3 Variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,2 +1,15 @@
-document.write("Where is my JS code?");
+//document.write("Where is my JS code?");
+//window.alert(5 + 6);
+//window.alert('5 + 6');
 
+/*
+const x = "web";
+const y = "mapping";
+const out = x + y;
+document.write(out);
+*/
+
+
+//Prompt Box used to prompt user to input username before entering website
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
