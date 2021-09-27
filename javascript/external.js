5eacf8b67eabeaa821ae5f87367e038aa466343a
aandishah <aandishah@gmail.com>
2021-09-26 21:33:57 -0400

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -1,15 +1,26 @@
+/*
+
 //document.write("Where is my JS code?");
 //window.alert(5 + 6);
 //window.alert('5 + 6');
 
-/*
+
 const x = "web";
 const y = "mapping";
 const out = x + y;
 document.write(out);
-*/
+
 
 
 //Prompt Box used to prompt user to input username before entering website
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
+
+
+*/
+
+x = 22;
+const y = 33;
+document.writeln(x + y) ;
+document.writeln("<br>"); 
+document.writeln(x += 33);
