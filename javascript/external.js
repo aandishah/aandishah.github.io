ba41382c9ddc3a18d50c2d323d1f0a1ce8d238ff
aandishah <aandishah@gmail.com>
2021-10-02 17:02:41 -0400

    Defined a function for the welcome message

--- javascript/external.js
+++ javascript/external.js
@@ -44,8 +44,25 @@ arr = ["Joe", "April", "Mark", "Sara"];
 for (var i=0; i < arr.length; i++)
 {
 	document.writeln(arr[i], "<br>")
-}*/
+}
 
-//Prompt Box used to prompt user to input username before entering website
+Prompt Box used to prompt user to input username before entering website
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
+*/
+
+function welcome()
+{
+	let a = "Please enter your name.";
+	let b = "Type your name here.";
+
+	//A prompt box is used to promp users to input a value before entering a page
+	user_name = window.prompt(a, b);
+
+	message = "<h1> Hello, welcome to my webpage, " + user_name + "!</h1>"
+
+	return message
+}
+
+document.write(welcome());
+
