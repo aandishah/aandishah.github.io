4bf1dbb460584b8013a7cd5a5e49074ee91ed6f7
aandishah <aandishah@gmail.com>
2021-10-05 21:16:00 -0400

    Added Calculator

--- javascript/external.js
+++ javascript/external.js
@@ -1,16 +1,3 @@
-/*var webmaps =
-[
-  ["Shipmap", 
-  "https://www.shipmap.org/",
-  "Shipmap.org is an ambitious interactive WebGL map of commercial shipping movements based on hundreds of millions of data points from throughout 2012 (the most recent year for which all the raw input data was available)."
-  ],
-  ["The Hate Map by Southern Poverty Law Center",
-  "https://www.splcenter.org/hate-map",
-  "The Hate Map on the spl website gives a quick and easy to follow visual narrative of where the hates groups across the US is located. It can be filtered by ideology, state, and relevant news to these groups are provided. "
-  ]  
-];*/
-
-
 var webmaps =
 [
   ["Shipmap", 
@@ -23,52 +10,43 @@ var webmaps =
   ]  
 ];
 
-
-function welcome()
-{
-	let a = "Please enter your name.";
-	let b = "Type your name here.";
-
-	//A prompt box is used to promp users to input a value before entering a page
-	user_name = window.prompt(a, b);
-
-	message = "<h1> Hello, welcome to my webpage, " + user_name + "!</h1>"
-
-	return message
-}
-
-/*function webmap_table()
-{
-  document.write("<table width=100%>"); 
-  for (var row=0; row < 2; row++)
-  {
-	document.write("<tr>");
-	for (var column=0; column < 3; column++)
-	{
-		document.write("<td>" + row + "," + column + "</td>");
-	}
-	document.write("</tr>");
-  }
-  document.write("</table>")
-}*/
-
 function webmap_table()
 {
   document.write("<table width=100%>"); 
   for (var row=0; row < webmaps.length; row++)
   {
-	document.write("<tr>");
-	for (var column=0; column < webmaps[0].length; column++)
-	{
-		document.write("<td>" + webmaps[rows][column] + "</td>");
-	}
-	document.write("</tr>");
+  document.write("<tr>");
+  for (var column=0; column < webmaps[0].length; column++)
+  {
+    document.write("<td>" + webmaps[row][column] + "</td>");
+  }
+  document.write("</tr>");
   }
   document.write("</table>")
 }
 
-
-function area_calculator()
+function welcome()
 {
-	document.write
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+
+  //A prompt box is used to promp users to input a value before entering a page
+  user_name = window.prompt(a, b);
+
+  message = "<h1> Hello, welcome to my webpage, " + user_name + "!</h1>"
+
+  return message
 }
+
+
+function AreaCalculator() {
+  var length;
+  var width;
+  var AreaCalc;
+  var result = document.getElementById('AreaCalc')
+  length = document.getElementById("length").value;
+  width = document.getElementById("width").value;     
+  AreaCalc = (length * width);
+   console.log(AreaCalc);
+   result.innerHTML = AreaCalc
+};
