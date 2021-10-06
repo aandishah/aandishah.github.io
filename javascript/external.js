05b7d8c495d860b9661fa982e877e9feab8523cf
aandishah <aandishah@gmail.com>
2021-10-05 22:22:47 -0400

    Completed Assignment 4

--- javascript/external.js
+++ javascript/external.js
@@ -1,19 +1,23 @@
 var webmaps =
 [
+  ["Name", 
+  "Link", 
+  "About"
+  ],
   ["Shipmap", 
   "https://www.shipmap.org/",
-  "Shipmap.org is an ambitious interactive WebGL map of commercial" 
+  "Shipmap.org is an ambitious interactive WebGL map of commercial \ shipping movements based on hundreds of millions of data points from throughout 2012 \ (the most recent year for which all the raw input data was available).\ The project's aim is to highlight for a broad audience the extraordinary \ scale of modern commercial shipping, the routes these huge vessels take around the world, \ the geographic spread of different types of cargo boats, \ and the amount of carbon dioxide they produce." 
   ],
   ["The Hate Map by Southern Poverty Law Center",
   "https://www.splcenter.org/hate-map",
-  "The Hate Map on the spl website gives a quick and easy to follow visual narrative"
+  "The Hate Map on the spl website gives a quick and easy to follow visual narrative \ narrative of where the hates groups across the US is located. \ . It can be filtered by ideology, state, and relevant news to these groups are provided. "
   ]  
 ];
 
 function webmap_table()
 {
   document.write("<table width=100%>"); 
-  for (var row=0; row < webmaps.length; row++)
+  for (var row=0; row < webmaps.length; row++) 
   {
   document.write("<tr>");
   for (var column=0; column < webmaps[0].length; column++)
@@ -39,13 +43,13 @@ function welcome()
 }
 
 
-function AreaCalculator() {
+function AreaCalc() {
   var length;
   var width;
   var AreaCalc;
   var result = document.getElementById('AreaCalc')
   length = document.getElementById("length").value;
-  width = document.getElementById("width").value;     
+  width = document.getElementById("width").value;  
   AreaCalc = (length * width);
    console.log(AreaCalc);
    result.innerHTML = AreaCalc
